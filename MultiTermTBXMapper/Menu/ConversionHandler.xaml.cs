e3f7ae8f4cf4f395e043b9958440679b3e813eff
James Schofield Hayes <james.s.hayes@gmail.com>
2017-06-12 11:24:15 -0600

    -Removed JSON in prep for JSON-free solution

--- MultiTermTBXMapper/Menu/ConversionHandler.xaml.cs
+++ MultiTermTBXMapper/Menu/ConversionHandler.xaml.cs
@@ -21,114 +21,13 @@ namespace MultiTermTBXMapper.Menu
             mappingDict = mapping;
 
             map();
-
-            Newtonsoft.Json.JsonSerializerSettings settings = new Newtonsoft.Json.JsonSerializerSettings();
-            
-            string json = Newtonsoft.Json.JsonConvert.SerializeObject(fullMapping, settings);
-
-            Console.Write(json);
         }
 
         private void map()
         {
-            foreach (string dc in mappingDict.Keys)
-            {
-                if (isConceptGrp(dc))
-                {
-                    handleConceptGrp(dc);
-                }
-
-                if (isLanguageGrp(dc))
-                {
-                    handleLanguageGrp(dc);
-                }
-
-                if (isTermGrp(dc))
-                {
-                    handleTermGrp(dc);
-                }
-            }
+            
 
 
-        }
-
-        private void handleConceptGrp(string dc)
-        {
-            //Handle all simple cases: no multiple tbx datcats, no picklists
-            if (mappingDict.getTBXMappingList(dc).Count < 2 || (!mappingDict.hasPicklist(dc) && !mappingDict.hasSplitContents(dc)))
-            {
-                TemplateSet ts = createTemplateSet(dc);
-                fullMapping.catMap["concept"].Add(dc, ts);
-            }
-            //Handle single tbx datcat with picklists
-            else if (mappingDict.hasPicklist(dc) && (mappingDict.getTBXMappingList(dc).Count < 2 || !mappingDict.hasSplitContents(dc)))
-            {
-                string tbx_dc = getTBXdc(dc);
-
-                string elt = tbxInfo[tbx_dc]?["element"];
-
-                string target = fullMapping.getTarget(elt);
-                string eltAtt = fullMapping.getEltAtt(elt, dc);
-
-
-            }
-        }
-
-        private void handleLanguageGrp(string dc)
-        {
-
-            //Handle all simple cases: no multiple tbx datcats, no picklists
-            if (mappingDict.getTBXMappingList(dc).Count < 2 || (!mappingDict.hasPicklist(dc) && !mappingDict.hasSplitContents(dc)))
-            {
-                TemplateSet ts = createTemplateSet(dc);
-
-
-                fullMapping.catMap["language"].Add(dc, ts);
-            }
-        }
-
-        private void handleTermGrp(string dc)
-        {
-            //Handle all simple cases: no multiple tbx datcats, no picklists
-            if (mappingDict.getTBXMappingList(dc).Count < 2 || (!mappingDict.hasPicklist(dc) && !mappingDict.hasSplitContents(dc)))
-            {
-                TemplateSet ts = createTemplateSet(dc);
-
-
-                fullMapping.catMap["term"].Add(dc, ts);
-            }
-        }
-
-        private string getTBXdc(string dc)
-        {
-            string tbx_dc = null;
-
-            foreach (string val in mappingDict.getTBXContentMap(dc)?.Values)
-            {
-                tbx_dc = val;
-                break;
-            }
-            if (tbx_dc == null)
-            {
-                tbx_dc = mappingDict.getTBXMappingList(dc)?[0];
-            }
-
-            return tbx_dc;
-        }
-
-        private TemplateSet createTemplateSet(string user_dc)
-        {
-            string tbx_dc = getTBXdc(user_dc);
-
-            string elt = tbxInfo[tbx_dc]?["element"];
-
-            string target = fullMapping.getTarget(elt);
-            string eltAtt = fullMapping.getEltAtt(elt, tbx_dc);
-
-            TemplateSet ts = new TemplateSet();
-            ((ts[0] as KeyList)[0] as Teasp).setAll(target, eltAtt);
-
-            return ts;
         }
 
         private bool isConceptGrp(string dc)
